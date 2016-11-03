c83628584aee0910bef4a0fc3da350090d0ecea0
Marmaduke Woodman <mmwoodman@gmail.com>
2016-11-03 13:47:19 +0100

    split test runner into script

---
+++ bin/run-tests.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# ignore extern and scripts under dev
+ignores=""
+for ign in extern bin/dev; do ignores="--ignore=$ign $ignores"; done
+
+# TODO adapt for different datasets, etc
+export SUBJECTS_DIR=`pwd`
+export SUBJECT='bnm'
+
+# maybe do coverage
+cov=""
+if [[ COV == "yes" ]]; then cov="--cov=bnm"; fi
+
+# run 'em
+py.test $cov $ignores
