0399b4736536a8d180ee5d08d89110c794b51f1e
liadomide <lia_domide@yahoo.com>
2016-11-09 10:05:46 +0200

    Keep all "main" scripts together

--- bin/snapshot.sh
+++ bin/snapshot.sh
@@ -35,34 +35,34 @@ if [ $# -gt 0 ]; then
         case "$2" in
 		    "2vols" )
                 refreshSnapshotNumber
-                python bnm.recon.qc.image.freeview.py coronal
+                python bnm.recon.qc.freeview.py coronal
                 freeview -v $3 $4:colormap=heat:opacity=0.3 -viewport coronal -layout 1 -cmd $slicesFileName
-                python bnm.recon.qc.image.freeview.py sagittal
+                python bnm.recon.qc.freeview.py sagittal
                 freeview -v $3 $4:colormap=heat:opacity=0.3 -viewport sagittal -layout 1 -cmd $slicesFileName
-                python bnm.recon.qc.image.freeview.py axial
+                python bnm.recon.qc.freeview.py axial
                 freeview -v $3 $4:colormap=heat:opacity=0.3 -viewport axial -layout 1 -cmd $slicesFileName
                 ;;
 		    "3vols" )
                 refreshSnapshotNumber
-                python bnm.recon.qc.image.freeview.py coronal
+                python bnm.recon.qc.freeview.py coronal
                 freeview -v $3 $4:colormap=heat:opacity=0.3 $5:colormap=jet:opacity=0.5 -viewport coronal -layout 1 -cmd $slicesFileName
-                python bnm.recon.qc.image.freeview.py sagittal
+                python bnm.recon.qc.freeview.py sagittal
                 freeview -v $3 $4:colormap=heat:opacity=0.3 $5:colormap=jet:opacity=0.5 -viewport sagittal -layout 1 -cmd $slicesFileName
-                python bnm.recon.qc.image.freeview.py axial
+                python bnm.recon.qc.freeview.py axial
                 freeview -v $3 $4:colormap=heat:opacity=0.3 $5:colormap=jet:opacity=0.5 -viewport axial -layout 1 -cmd $slicesFileName
                 ;;
 		    "surf_annot" )
                 refreshSnapshotNumber
-                python bnm.recon.qc.image.freeview.py surface_annotation
+                python bnm.recon.qc.freeview.py surface_annotation
                 freeview -f $3:annot=$4 -viewport 3D -cmd $cameraPositionsFileName
                 ;;
 		    "vol_surf" )
                 refreshSnapshotNumber
-                python bnm.recon.qc.image.freeview.py coronal
+                python bnm.recon.qc.freeview.py coronal
                 freeview -v $3 -f $4 -viewport coronal -cmd $slicesFileName
-                python bnm.recon.qc.image.freeview.py sagittal
+                python bnm.recon.qc.freeview.py sagittal
                 freeview -v $3 -f $4 -viewport sagittal -cmd $slicesFileName
-                python bnm.recon.qc.image.freeview.py axial
+                python bnm.recon.qc.freeview.py axial
                 freeview -v $3 -f $4 -viewport axial -cmd $slicesFileName
                 ;;
 		    "vol_white_pial" )
@@ -73,12 +73,12 @@ if [ $# -gt 0 ]; then
                     value=""
                 fi
                 refreshSnapshotNumber
-                python bnm.recon.qc.image.freeview.py coronal
+                python bnm.recon.qc.freeview.py coronal
                 echo $SURF/lh.white$value
                 freeview -v $3 -f $SURF/{lh,rh}.{white$value:edgecolor=yellow,pial$value:edgecolor=red} -viewport coronal -cmd $slicesFileName
-                python bnm.recon.qc.image.freeview.py sagittal
+                python bnm.recon.qc.freeview.py sagittal
                 freeview -v $3 -f $SURF/{lh,rh}.{white$value:edgecolor=yellow,pial$value:edgecolor=red} -viewport sagittal -cmd $slicesFileName
-                python bnm.recon.qc.image.freeview.py axial
+                python bnm.recon.qc.freeview.py axial
                 freeview -v $3 -f $SURF/{lh,rh}.{white$value:edgecolor=yellow,pial$value:edgecolor=red} -viewport axial -cmd $slicesFileName
                 ;;
 	    esac
@@ -91,7 +91,7 @@ if [ $# -gt 0 ]; then
         #   python setup.py develop/install
         source activate bnm-recon
         refreshSnapshotNumber
-        python -m bnm.recon.qc "$@"
+        python bnm.recon.qc.snapshot.py "$@"
 
     fi
 
