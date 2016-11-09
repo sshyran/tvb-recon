01a548ccc540164c6643fa55521a4e8aa1f56b31
liadomide <lia_domide@yahoo.com>
2016-11-09 16:31:01 +0200

    Fix travis run

--- bnm/recon/logger.py
+++ bnm/recon/logger.py
@@ -9,10 +9,9 @@ OUTPUT_FOLDER = "output"
 LOG_FILE = os.path.join(OUTPUT_FOLDER, 'bnm.log')
 
 
-def create_log_file():
-    if not os.path.exists(LOG_FILE):
-        if not os.path.exists(OUTPUT_FOLDER):
-            os.mkdir(OUTPUT_FOLDER)
+def _ensure_log_folder_exists():
+    if not os.path.exists(OUTPUT_FOLDER):
+        os.mkdir(OUTPUT_FOLDER)
 
 
 def get_logger(parent_module):
@@ -22,8 +21,8 @@ def get_logger(parent_module):
     in case we need to refactor in the future.
     """
     log_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    _ensure_log_folder_exists()
 
-    create_log_file()
     file_handler = logging.FileHandler(LOG_FILE)
     file_handler.setLevel(logging.DEBUG)
     file_handler.setFormatter(log_formatter)
