a396580a10c6dae8e5673dbde6d1329265b6df03
popaula937 <paula.popa@codemart.ro>
2016-11-14 11:29:56 +0200

    Mihai's review for Annotation model and parser.

--- bnm/recon/qc/parser/annotation.py
+++ bnm/recon/qc/parser/annotation.py
@@ -7,6 +7,5 @@ from bnm.recon.qc.model.annotation import Annotation
 class AnnotationParser(object):
 
     def parse(self, annotation_path):
-        [region_mapping, regions_color_table, region_names] = read_annot(annotation_path)
-
+        region_mapping, regions_color_table, region_names = read_annot(annotation_path)
         return Annotation(region_mapping, regions_color_table, region_names)
