d0018d9a4d4f8f597201887062c0429eabf85659
popaula937 <paula.popa@codemart.ro>
2016-11-18 15:16:35 +0200

    Added tests for volume reader.

---
+++ bnm/tests/qc/test_volumes.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+from bnm.recon.qc.parser.volume import VolumeParser
+from bnm.tests.base import get_data_file
+from nibabel.filebasedimages import ImageFileError
+from nibabel.py3k import FileNotFoundError
+
+TEST_SUBJECT = 'freesurfer_fsaverage'
+TEST_VOLUME_FOLDER = 'mri'
+
+
+def test_parse_volume():
+    parser = VolumeParser()
+    file_path = get_data_file(TEST_SUBJECT, TEST_VOLUME_FOLDER, "T1.nii.gz")
+    volume = parser.parse(file_path)
+    assert volume.dimensions == (256, 256, 256)
+
+
+def test_parse_not_existing_volume():
+    parser = VolumeParser()
+    file_path = "not-existent-volume.nii.gz"
+    with pytest.raises(FileNotFoundError):
+        parser.parse(file_path)
+
+
+def test_parse_not_volume():
+    parser = VolumeParser()
+    file_path = get_data_file(TEST_SUBJECT, "surf", "lh.pial")
+    with pytest.raises(ImageFileError):
+        parser.parse(file_path)
