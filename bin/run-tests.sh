a1fde8e78a5ee2b597aaee124f7c420502811837
popaula937 <paula.popa@codemart.ro>
2016-11-21 15:31:27 +0200

    Small change to generate coverage.

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -10,7 +10,7 @@ export SUBJECT='bnm'
 
 # maybe do coverage
 cov=""
-if [[ COV == "yes" ]]; then cov="--cov=bnm"; fi
+if [[ $COV == "yes" ]]; then cov="--cov=bnm"; fi
 
 # run 'em
 py.test $cov $ignores
