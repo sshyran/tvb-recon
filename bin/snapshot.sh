6049b2cf2e8c3bc6434d54e8b29719710b6e4cd3
popaula937 <paula.popa@codemart.ro>
2016-11-24 17:57:33 +0200

    Moved the vox2ras matrix reading to python scripts and made the necessary changes for it.

--- bin/snapshot.sh
+++ bin/snapshot.sh
@@ -8,22 +8,14 @@ function refreshSnapshotNumber {
 
 if [ $# -gt 0 ]; then
 
-	export SNAPSHOT_NUMBER
-
 	if [ -z $SNAPSHOT_NUMBER ]; then
-		SNAPSHOT_NUMBER=0
+			export SNAPSHOT_NUMBER=0
 	fi
 
-    if [ -z SURF ]; then
-        export SURF=$SUBJ_DIR/'surf'
-    fi
-
-	matrixFileName="matrix.txt"
-	# TODO matrix should be read from background volume
-	mri_info --vox2ras $SUBJ_DIR/mri/T1.mgz --o $matrixFileName
-
-
     if [ "$1" == "use_freeview" ]; then
+
+        matrixFileName="matrix.txt"
+	    mri_info --vox2ras $SUBJ_DIR/mri/T1.mgz --o $matrixFileName
 
         screenshotName="snapshot"
 	    slicesFileName="slices.txt"
@@ -35,20 +27,20 @@ if [ $# -gt 0 ]; then
         case "$2" in
 		    "2vols" )
                 refreshSnapshotNumber
-                python bnm.recon.qc.freeview.py coronal
+                python -m bnm.recon.qc.freeview coronal
                 freeview -v $3 $4:colormap=heat:opacity=0.3 -viewport coronal -layout 1 -cmd $slicesFileName
-                python bnm.recon.qc.freeview.py sagittal
+                python -m bnm.recon.qc.freeview sagittal
                 freeview -v $3 $4:colormap=heat:opacity=0.3 -viewport sagittal -layout 1 -cmd $slicesFileName
-                python bnm.recon.qc.freeview.py axial
+                python -m bnm.recon.qc.freeview axial
                 freeview -v $3 $4:colormap=heat:opacity=0.3 -viewport axial -layout 1 -cmd $slicesFileName
                 ;;
 		    "3vols" )
                 refreshSnapshotNumber
-                python bnm.recon.qc.freeview.py coronal
+                python -m bnm.recon.qc.freeview coronal
                 freeview -v $3 $4:colormap=heat:opacity=0.3 $5:colormap=jet:opacity=0.5 -viewport coronal -layout 1 -cmd $slicesFileName
-                python bnm.recon.qc.freeview.py sagittal
+                python -m bnm.recon.qc.freeview sagittal
                 freeview -v $3 $4:colormap=heat:opacity=0.3 $5:colormap=jet:opacity=0.5 -viewport sagittal -layout 1 -cmd $slicesFileName
-                python bnm.recon.qc.freeview.py axial
+                python -m bnm.recon.qc.freeview axial
                 freeview -v $3 $4:colormap=heat:opacity=0.3 $5:colormap=jet:opacity=0.5 -viewport axial -layout 1 -cmd $slicesFileName
                 ;;
 		    "surf_annot" )
@@ -58,44 +50,45 @@ if [ $# -gt 0 ]; then
                 ;;
 		    "vol_surf" )
                 refreshSnapshotNumber
-                python bnm.recon.qc.freeview.py coronal
+                python -m bnm.recon.qc.freeview coronal
                 freeview -v $3 -f $4 -viewport coronal -cmd $slicesFileName
-                python bnm.recon.qc.freeview.py sagittal
+                python -m bnm.recon.qc.freeview sagittal
                 freeview -v $3 -f $4 -viewport sagittal -cmd $slicesFileName
-                python bnm.recon.qc.freeview.py axial
+                python -m bnm.recon.qc.freeview axial
                 freeview -v $3 -f $4 -viewport axial -cmd $slicesFileName
                 ;;
 		    "vol_white_pial" )
-		    echo $#
+		        if [ -z SURF ]; then
+                    export SURF=$SUBJ_DIR/'surf'
+                fi
+
                 if [ $# -gt 3 and $4 == "-resampled_surface_name" ]; then
                     value=".$5"
                 else
                     value=""
                 fi
                 refreshSnapshotNumber
-                python bnm.recon.qc.freeview.py coronal
-                echo $SURF/lh.white$value
+                python -m bnm.recon.qc.freeview coronal
                 freeview -v $3 -f $SURF/{lh,rh}.{white$value:edgecolor=yellow,pial$value:edgecolor=red} -viewport coronal -cmd $slicesFileName
-                python bnm.recon.qc.freeview.py sagittal
+                python -m bnm.recon.qc.freeview sagittal
                 freeview -v $3 -f $SURF/{lh,rh}.{white$value:edgecolor=yellow,pial$value:edgecolor=red} -viewport sagittal -cmd $slicesFileName
-                python bnm.recon.qc.freeview.py axial
+                python -m bnm.recon.qc.freeview axial
                 freeview -v $3 -f $SURF/{lh,rh}.{white$value:edgecolor=yellow,pial$value:edgecolor=red} -viewport axial -cmd $slicesFileName
                 ;;
 	    esac
 	    rm -r $slicesFileName
 	    rm -r $cameraPositionsFileName
+	    rm -r $matrixFileName
 
     else
 
         #    It needs to have bnm.recon in Python Path
         #   python setup.py develop/install
-        source activate bnm-recon
+        #   source activate bnm-recon
         refreshSnapshotNumber
-        python bnm.recon.qc.snapshot.py "$@"
+        python -m bnm.recon.qc.snapshot "$@"
 
     fi
-
-    rm -r $matrixFileName
 
 else
 
