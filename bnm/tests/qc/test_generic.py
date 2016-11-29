b95aa388f850ab0f931de2e1de72e9bd065381e7
popaula937 <paula.popa@codemart.ro>
2016-11-29 15:08:28 +0200

    Changed path for cc_point in test_generic.

--- bnm/tests/qc/test_generic.py
+++ bnm/tests/qc/test_generic.py
@@ -6,6 +6,6 @@ from bnm.tests.base import get_data_file
 
 def test_read_cc_point():
     parser = GenericParser()
-    file_path = get_data_file("cc_point.txt")
+    file_path = get_data_file("fsaverage_modified", "scripts", "ponscc.cut.log")
     cc_point = parser.read_cc_point(file_path, GenericParser.point_line_flag)
-    assert cc_point == [0.0, 0.0, 0.0, 1]
+    assert cc_point == [100.0, 100.0, 100.0, 1]
