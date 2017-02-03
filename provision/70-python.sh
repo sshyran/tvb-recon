8833fe6aba1fc29ce8c1a0aa85f76ab8595f5819
Marmaduke Woodman <mmwoodman@gmail.com>
2017-02-03 12:03:42 +0100

    add autopep8 to py env

--- provision/70-python.sh
+++ provision/70-python.sh
@@ -49,7 +49,7 @@ done
 
 py_pkgs="numpy scipy matplotlib cython scikit-learn pandas h5py nibabel"
 py_pkgs="$py_pkgs nibabel anytree trimesh flake8 mypy mne jupyterlab"
-py_pkgs="$py_pkgs gdist pytest pytest-cov"
+py_pkgs="$py_pkgs gdist pytest pytest-cov autopep8"
 
 for pkg in $py_pkgs
 do
