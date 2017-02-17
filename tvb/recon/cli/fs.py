9eecd82ed558beb278d492568723973186674f90
Viktor Sip <viktor.sip@univ-amu.fr>
2017-02-17 15:20:13 +0100

    recon-all basic flags

--- tvb/recon/cli/fs.py
+++ tvb/recon/cli/fs.py
@@ -208,7 +208,40 @@ class recon_all(BaseFsCLI):
     The recon_all command from the fs package.
 
     """
-    exe = 'recon_all'
+    exe = 'recon-all'
+
+    class Flags(BaseFsCLI.Flags):
+        subjid = '-subjid'
+        input_ = '-i'
+        all_ = '-all'
+        parallel = '-parallel'
+        openmp = '-openmp'
+
+
+def reconstruct(subjid: str,
+                input_,
+                all_: bool=True,
+                parallel: bool=False,
+                openmp: int=4
+                ):
+    """
+    Perform the FreeSurfer reconstruction process.
+
+    """
+
+    args = [
+        recon_all.exe,
+        recon_all.Flags.subjid, subjid,
+        recon_all.Flags.input_, input_
+    ]
+    if all_:
+        args += [recon_all.Flags.all_]
+    if parallel:
+        args += [recon_all.Flags.parallel]
+        args += [recon_all.Flags.openmp, openmp]
+
+    return args
+
 
 
 def convert(in_, out,
