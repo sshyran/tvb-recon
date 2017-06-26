4de0f960c5aa6858e4cabd1e98826e1211fa0b16
popaula937 <paula.popa@codemart.ro>
2017-06-26 13:36:01 +0300

    Added main that converts a .tck file (Mrtrix specific format) to .trk format that TVB knows to read. This is linked to TVB-1971 task (http://req.thevirtualbrain.org/browse/TVB-1971).

---
+++ tvb/recon/io/tck2trk.py
@@ -0,0 +1,19 @@
+import sys
+
+if len(sys.argv) < 3:
+    print("Loads NiPy module to transform .tck file to .trk file")
+    print("Dependencies: nipype, dipy, nibabel")
+    print("Usage:", sys.argv[0], "<base_image> <input_file>")
+    print("""base_image - file with resolution of template (T1.nii.gz)
+             input_file - MRTrix track file (.tck)""")
+    print("Output: TrackVis track vile (.trk)")
+    sys.exit(0)
+
+import nipype.interfaces.mrtrix as mrt
+
+mr = mrt.MRTrix2TrackVis()
+mr.inputs.image_file = sys.argv[1]
+mr.inputs.in_file = sys.argv[2]
+mr.inputs.out_filename = sys.argv[2].split('.')[-2] + '.trk'
+mr.run()
+mr.inputs.print_traits()
