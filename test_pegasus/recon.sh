c7a68122ec5cd6a29e9c6e482790ab76988db0f6
popaula937 <paula.popa@codemart.ro>
2017-07-28 14:59:14 +0300

    Added all steps for recon job.

--- test_pegasus/recon.sh
+++ test_pegasus/recon.sh
@@ -6,7 +6,8 @@ source "/WORK/FS_NEW/freesurfer/SetUpFreeSurfer.sh"
 
 f=$PWD
 
-/WORK/FS_NEW/freesurfer/bin/recon-all -autorecon1 -s $1 -i $2
+/WORK/FS_NEW/freesurfer/bin/recon-all -all -parallel -openmp $3 -s $1 -i $2
 
 cd $FREESURFER_HOME/subjects/$1/mri
 cp T1.mgz $f
+cp aparc+aseg.mgz $f
