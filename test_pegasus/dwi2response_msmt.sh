bc02f030fe1655f5e979e0eb26f463bc152a4a09
popaula937 <paula.popa@codemart.ro>
2017-07-28 15:02:14 +0300

    Exec file for dwi multi shell.

---
+++ test_pegasus/dwi2response_msmt.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+export HOME=/Users/pipeline
+export PATH=/WORK/MRtrix/mrtrix3/release/bin:/WORK/MRtrix/mrtrix3/scripts:${PATH}
+
+/WORK/MRtrix/mrtrix3/scripts/dwi2response msmt_5tt $1 $2 $3 $4 $5 -voxels $6 -nthreads $7
