5c4dc4d61ad15cfa55e4100d5ab3ffb96522e88c
popaula937 <paula.popa@codemart.ro>
2017-08-01 14:12:53 +0300

    Script for bbregister.

---
+++ test_pegasus/bbregister.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+export HOME=/Users/pipeline
+export FREESURFER_HOME="/WORK/FS_NEW/freesurfer"
+source "/WORK/FS_NEW/freesurfer/SetUpFreeSurfer.sh"
+export FSL_DIR="/WORK/FSL/fsl"
+source ${FSL_DIR}/etc/fslconf/fsl.sh
+
+bbregister --s $1 --mov $2 --o $3 --dti --reg $4 --lta $5 --fslmat $6
