4d1a001676e0c17d973280b73ff0ab791b122f3b
popaula937 <paula.popa@codemart.ro>
2017-08-03 15:48:43 +0300

    Changed the way bash args are used.

--- test_pegasus/aseg_concatenation.sh
+++ test_pegasus/aseg_concatenation.sh
@@ -1,18 +1,9 @@
 #!/usr/bin/env bash
 
-# TODO find a better way
-f=$PWD
-s=$f/$1
-a=$f/$2
-l=$f/$4
-
-
 source //anaconda/bin/activate tvb_recon_python3_env
 
 export FREESURFER_HOME="/WORK/FS_NEW/freesurfer"
 source "/WORK/FS_NEW/freesurfer/SetUpFreeSurfer.sh"
 export SUBJECT="TVB2PEG22"
 
-cd /WORK/BNM/tvb-recon/tvb-recon
-
-python -c "import tvb.recon.algo.reconutils; tvb.recon.algo.reconutils.aseg_surf_conc_annot('$f','$s','$a','$3',lut_path='$l')"
+python -c "import tvb.recon.algo.reconutils; tvb.recon.algo.reconutils.aseg_surf_conc_annot('$f','$1','$2','$3',lut_path='$4')"
--- test_pegasus/convert_output.sh
+++ test_pegasus/convert_output.sh
@@ -1,16 +1,8 @@
 #!/usr/bin/env bash
 
-# TODO find a better way
-f=$PWD
-w=$f/$1
-t=$f/$2
-l=$f/$3
-
-
 source //anaconda/bin/activate tvb_recon_python3_env
 
 export FREESURFER_HOME="/WORK/FS_NEW/freesurfer"
 source "/WORK/FS_NEW/freesurfer/SetUpFreeSurfer.sh"
 
-cd /WORK/BNM/tvb-recon/tvb-recon
-python -m tvb.recon.qc.tvb_output $f $w $t $l $f
+python -m tvb.recon.qc.tvb_output $PWD $1 $2 $3 $PWD
