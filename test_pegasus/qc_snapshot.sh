6a720cef1bdc0a891b77bb7f24068d6f05624824
popaula937 <paula.popa@codemart.ro>
2017-08-03 15:44:14 +0300

    First version of snapshots class.

---
+++ test_pegasus/qc_snapshot.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+source //anaconda/bin/activate tvb_recon_python3_env
+
+export FREESURFER_HOME="/WORK/FS_NEW/freesurfer"
+source "/WORK/FS_NEW/freesurfer/SetUpFreeSurfer.sh"
+export SUBJECT="TVB2PEG30"
+export FIGS=$PWD
+export SNAPSHOT_NUMBER=$1
+
+python -m tvb.recon.qc.snapshot ${@:2}
