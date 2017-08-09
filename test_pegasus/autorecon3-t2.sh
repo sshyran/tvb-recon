45d73afcdc07d79981e786da55aaddb6906fde72
popaula937 <paula.popa@codemart.ro>
2017-08-09 17:15:39 +0300

    Small fixes.

--- test_pegasus/autorecon3-t2.sh
+++ test_pegasus/autorecon3-t2.sh
@@ -10,3 +10,11 @@ recon-all -autorecon3 -T2 $2 -T2pial -parallel -openmp $3 -s $1
 
 cd ${FREESURFER_HOME}/subjects/$1/mri
 cp aparc+aseg.mgz $f
+
+cd ../surf
+cp lh.pial $f
+cp rh.pial $f
+
+cd ../label
+cp lh.aparc.annot $f
+cp rh.aparc.annot $f
--- test_pegasus/flirt-reversed.sh
+++ test_pegasus/flirt-reversed.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 export FSL_DIR
+export PATH=${FSL_DIR}/bin:${PATH}
 source ${FSL_DIR}/etc/fslconf/fsl.sh
 
 flirt -applyxfm -in $1 -ref $2 -out $3 -init $4
