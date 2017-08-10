3724825f72e5518d102ab8ba94b92a51289f0127
popaula937 <paula.popa@codemart.ro>
2017-08-10 12:03:59 +0300

    Made some changes to tvb_output main in order to parse arguments nicer and have a separate call for pegasus.

--- test_pegasus/convert_output.sh
+++ test_pegasus/convert_output.sh
@@ -5,4 +5,4 @@ source //anaconda/bin/activate tvb_recon_python3_env
 export FREESURFER_HOME
 source ${FREESURFER_HOME}/SetUpFreeSurfer.sh
 
-python -m tvb.recon.qc.tvb_output $PWD $1 $2 $3 $PWD
+python -m tvb.recon.qc.tvb_output -p $PWD $1 $2 $3 $PWD
