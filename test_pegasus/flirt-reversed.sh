e2bfd6a760212b641adddc203787adc4d87dc985
popaula937 <paula.popa@codemart.ro>
2017-08-10 12:00:49 +0300

    Added interpolation flag to reversed flirt.

--- test_pegasus/flirt-reversed.sh
+++ test_pegasus/flirt-reversed.sh
@@ -4,4 +4,4 @@ export FSL_DIR
 export PATH=${FSL_DIR}/bin:${PATH}
 source ${FSL_DIR}/etc/fslconf/fsl.sh
 
-flirt -applyxfm -in $1 -ref $2 -out $3 -init $4
+flirt -applyxfm -in $1 -ref $2 -out $3 -init $4 -interp nearestneighbour
