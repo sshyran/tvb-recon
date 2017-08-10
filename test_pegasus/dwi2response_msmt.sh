6ebcc8c1fdc20cecd39df646bbb8e76d8a47958c
popaula937 <paula.popa@codemart.ro>
2017-08-10 17:00:01 +0300

    Used env var instead of path.

--- test_pegasus/dwi2response_msmt.sh
+++ test_pegasus/dwi2response_msmt.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export HOME=/Users/pipeline
-export PATH=/WORK/MRtrix/mrtrix3/release/bin:/WORK/MRtrix/mrtrix3/scripts:${PATH}
+export HOME
+export PATH=${MRTRIX_BIN}:${MRTRIX_SCRIPTS}:${PATH}
 
 dwi2response msmt_5tt $1 $2 $3 $4 $5 -voxels $6 -nthreads $7
