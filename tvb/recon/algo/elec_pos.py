51d61dd8bd7e140da84c5ffa66d28dc037f0e100
dionperd <dionperd@gmail.com>
2018-03-01 08:19:05 +0100

    Ongoing work on ele_pos.py

--- tvb/recon/algo/elec_pos.py
+++ tvb/recon/algo/elec_pos.py
@@ -1,9 +1,6 @@
 import numpy as np
 import os
-from tvb.recon.algo.service.utils import execute_command
-
-import nibabel as nib
-
+from tvb.recon.algo.service.utils import compute_affine_transform, execute_command
 from tvb.recon.algo.service.volume import VolumeService
 from tvb.recon.algo.service.sensor import SensorService
 
@@ -25,130 +22,38 @@ def save_xyz_file(coords, names, filename):
             fl.write("%s %8.2f %8.2f %8.2f\n" % (name, coord[0], coord[1], coord[2]))
 
 
-
-
-# This function creates a new volume matrix of similar shape to a reference volume volume matrix
-# by setting input values at input positions (optionally + & - (int) dist)
-# after applying to them the ref_aff transform
-def gen_volume_points(values, positions, ref_volume, ref_aff, dist=0):
-    new_volume = np.zeros(ref_volume.shape)
-    new_volume[:, :] = np.nan
-
-    kx, ky, kz = np.mgrid[-dist:dist + 1, -dist:dist + 1, -dist:dist + 1]
-
-    for val, pos in zip(values, positions):
-        ix, iy, iz = np.linalg.solve(ref_aff, np.append(pos, 1.0))[0:3].astype(int)
-        # new_volume[inds[0], inds[1], inds[2]] = val
-        new_volume[ix + kx, iy + ky, iz + kz] = val
-
-    return new_volume
-
-
-# Create and save a new nifti label volume of similar shape to a reference volume
-# by setting input values at input positions (optionally + & - (int) dist)
-# after applying to them the ref_aff transform
-def save_nifti_points(values, names, position_file, ref_volume_file, out_file, skip_missing=False, dist=0):
-    ref_nii = nib.load(ref_volume_file)
-
-    contact_names = list(np.genfromtxt(position_file, dtype=str, usecols=(0,)))
-    contact_positions = np.genfromtxt(position_file, dtype=float, usecols=(1, 2, 3))
-
-    positions = np.zeros((len(values), 3))
-    for i, name in enumerate(names):
-        try:
-            contact_ind = contact_names.index(name)
-            pos = contact_positions[contact_ind, :]
-        except ValueError:
-            pos = np.array([np.nan, np.nan, np.nan])
-
-        positions[i, :] = pos
-
-    missing_mask = np.isnan(positions[:, 0])
-    if skip_missing:
-        values = values[~missing_mask]
-        positions = positions[~missing_mask, :]
-    else:
-        if np.any(missing_mask):
-            raise ValueError("Missing contact position(s) for: %s." % ", ".join([
-                name for name, missing in zip(names, missing_mask) if missing]))
-
-        # np.array(names)[missing_mask]))
-
-    new_volume = gen_volume_points(values, positions, ref_nii.get_data(), ref_nii.affine, dist=dist)
-    # add_min_max(new_volume)
-
-    new_nii = nib.Nifti1Image(new_volume, ref_nii.affine)
-    nib.save(new_nii, out_file)
-
-
-# Read the seeg contacts' names and positions from pom file and save then in text and nifti files:
+# Read the seeg contacts' labels and positions from pom file and save then in text and nifti files:
 def read_write_pom_files(pomfile, elec_file_pom, mrielec, elec_nii_pom):
+    """
+    # NEEDED ONLY FOR CC PATIENTS!
+    # Read the seeg contacts' labels and positions from pom file and save then in text and nifti files:
+
+    :param pomfile:
+    :param elec_file_pom:
+    :param mrielec:
+    :param elec_nii_pom:
+    :return:
+    """
     with open(pomfile) as fl:
         lines = fl.readlines()
 
     loc_start = find_line_starting_with(lines, "LOCATION_LIST START_LIST") + 1
     loc_end = find_line_starting_with(lines, "LOCATION_LIST END_LIST")
 
-    names_start = find_line_starting_with(lines, "REMARK_LIST START_LIST") + 1
-    names_end = find_line_starting_with(lines, "REMARK_LIST END_LIST")
+    labels_start = find_line_starting_with(lines, "REMARK_LIST START_LIST") + 1
+    labels_end = find_line_starting_with(lines, "REMARK_LIST END_LIST")
 
     coords_list = lines[loc_start:loc_end]
-    names = lines[names_start:names_end]
+    labels = lines[labels_start:labels_end]
 
     coords_list = np.array([list(map(float, coords.split("\t"))) for coords in coords_list])
-    names = [name.strip() for name in names]
+    labels = [label.strip() for label in labels]
 
-    save_xyz_file(coords_list, names, elec_file_pom)
-    save_nifti_points(np.ones(len(names)), names, elec_file_pom, mrielec, elec_nii_pom, dist=1)
+    save_xyz_file(coords_list, labels, elec_file_pom)
+    volume_service.gen_label_volume_from_coords(np.ones(len(labels)), np.array(coords_list), labels, mrielec,
+                                                elec_nii_pom, skip_missing=False, dist=1)
 
-    return names, coords_list
-
-
-# TODO: an automatic way to pick contact's coordinates randomly or one per electrode from the seeg_pom file
-coords_from = np.array([
-    [-20.59, 19.51, 38.49],
-    [-71.79, 10.24, 38.51],
-    [2.95, 43.21, 105.8],
-    [-48.5, 22.58, 104.8],
-    [-0.69, 74.26, 26.77],
-    [-60.11, 70.55, 30.76],
-    [3.822, -10.42, 100.8]
-])
-
-# TODO: an automatic way to pick the corresponding contacts'coordinates from the mri_electrode volume
-coords_to = np.array([
-    [-21.25, 16.31, 29.61],
-    [-72.75, 7.59, 28.61],
-    [1.882, 29.46, 99.93],
-    [-50.33, 9.83, 95.7],
-    [-2.35, 71.61, 27.54],
-    [-60.82, 68.77, 30.01],
-    [1.705, -22.75, 87.75]
-])
-
-# Find the affine transformation pomfile -> MRIelectrodes
-def pom_in_mrielec_transform(coords_from, coords_to):
-
-    # Pad the data with ones, so that our transformation can do translations too
-    n = coords_from.shape[0]
-    pad = lambda x: np.hstack([x, np.ones((x.shape[0], 1))])
-    unpad = lambda x: x[:, :-1]
-    X = pad(coords_from)
-    Y = pad(coords_to)
-
-    # Solve the least squares problem X * A = Y
-    # to find our transformation matrix A
-    A, res, rank, s = np.linalg.lstsq(X, Y)
-
-    aff_transform = lambda x: unpad(np.dot(pad(x), A))
-
-    print("Target:")
-    print(coords_to)
-    print("Result:")
-    print(aff_transform(coords_from))
-    print("Max error:", np.abs(coords_to - aff_transform(coords_from)).max())
-
-    return aff_transform
+    return labels, coords_list
 
 
 # Transform the coordinates along (pom file ->) mri_elec -> t1
@@ -160,8 +65,9 @@ def transform(elec_file_pom, mri_elec, t1, elec_file_t1, elec_nii_t1, transform_
     coords_file = os.path.join(dirname, "seeg_coords.xyz")
     np.savetxt(coords_file, coords, fmt='%.3e', delimiter=' ')
     coords_file_t1 = os.path.join(dirname, "seeg_coords_in_t1.xyz")
-    coords_in_t1, coords_file_t1 = volume_service.transform(coords_file, mri_elec, t1, transform_mat,
-                                                            coords_file_t1)
+    # TODO correct it to get coords_in_t1
+    coords_in_t1, coords_file_t1 = volume_service.transform_coords(coords_file, mri_elec, t1, transform_mat,
+                                                                   coords_file_t1)
     # output, std_out, time = \
     #     execute_command("img2imgcoord %s -mm -src %s -dest %s -xfm %s > %s" \
     #                         % (coords_file, src_img, dest_img, transform_mat, coords_file_t1),
@@ -174,16 +80,10 @@ def transform(elec_file_pom, mri_elec, t1, elec_file_t1, elec_nii_t1, transform_
         newlines.append(label + " " + line)
     with open(elec_file_t1, "w") as fl:
         fl.writelines(newlines)
-    volume_service.gen_label_volume_from_coords(np.ones(len(labels)), coords_in_t1, labels, t1, elec_nii_t1,
+    volume_service.gen_label_volume_from_coords(np.ones(len(labels)), elec_file_t1, labels, t1, elec_nii_t1,
                                                 skip_missing=False, dist=1)
-    save_nifti_points(np.ones(len(labels)), labels, elec_file_t1, t1, elec_nii_t1, dist=1)
-    # # Originally in Viktor's code (but I was getting an error that there is no run attribute for module...:
-    # cp = subprocess.run("echo %s | img2imgcoord -mm -src %s -dest %s -xfm %s" \
-    #                     % (coords_str, src_img, dest_img, transform_mat),
-    #                     shell=True, stdout=subprocess.PIPE)
-    # transformed_coords_str = cp.stdout.decode('ascii').strip().split('\n')[-1]
-    # transformed_coords =  np.array([float(x) for x in transformed_coords_str.split(" ") if x])
-
+    # volume_service.gen_label_volume_from_coords(np.ones(len(labels)), coords_in_t1, labels, t1, elec_nii_t1,
+    #                                             skip_missing=False, dist=1)
 
 
 def main_elec_pos(patient, POM_TO_MRIELEC_TRNSFRM=False):
@@ -219,10 +119,49 @@ def main_elec_pos(patient, POM_TO_MRIELEC_TRNSFRM=False):
     # B. Optional & the ONLY MANUAL job in case the pom coordinates are not in the same space as the MRIelectrode volume
     # Find the affine transformation pomfile -> MRIelectrodes
     if POM_TO_MRIELEC_TRNSFRM:
-        aff_transform = pom_in_mrielec_transform()
+        # TODO: an automatic way to pick contact's coordinates randomly or one per electrode from the seeg_pom file
+        coords_from = np.array([
+            [-20.59, 19.51, 38.49],
+            [-71.79, 10.24, 38.51],
+            [2.95, 43.21, 105.8],
+            [-48.5, 22.58, 104.8],
+            [-0.69, 74.26, 26.77],
+            [-60.11, 70.55, 30.76],
+            [3.822, -10.42, 100.8]
+        ])
+
+        # TODO: an automatic way to pick the corresponding contacts'coordinates from the mri_electrode volume
+        coords_to = np.array([
+            [-21.25, 16.31, 29.61],
+            [-72.75, 7.59, 28.61],
+            [1.882, 29.46, 99.93],
+            [-50.33, 9.83, 95.7],
+            [-2.35, 71.61, 27.54],
+            [-60.82, 68.77, 30.01],
+            [1.705, -22.75, 87.75]
+        ])
+        aff_transform = compute_affine_transform(coords_from, coords_to)
+
+        # # Alternative way:
+        # pom_to_mrielec = os.path.join(elec_folder, "pom_to_mrielec.mat")
+        # pom_in_mrielec = os.path.join(elec_folder, "pom_in_mrielec.nii.gz")
+        #
+        # regopts = "-cost mutualinfo -dof 12 -searchrz -180 180 -searchry -180 180  -searchrx -180 180"
+        # execute_command("flirt " + regopts +
+        #                 " -in " + elec_nii_pom +
+        #                 " -inweight " + elec_nii_pom +
+        #                 " -ref " + mrielec +
+        #                 " -refweight " + mrielec +
+        #                 " -omat " + pom_to_mrielec +
+        #                 " -out " + pom_in_mrielec)
+        #
+        # elec_file_mrielec = os.path.join(elec_folder, "seeg_pom_in_mrielec.xyz")
+        # transform(elec_file_pom, elec_nii_pom, mrielec, elec_file_mrielec, pom_in_mrielec, pom_to_mrielec)
+
     else:
         aff_transform = None
 
+    #
     # C. Optional job in case we haven't computed already the MRIelec_in_t1 transformation matrix
     #    (This must be already part of the workflow...)
     if not os.path.isfile(mrielec_to_t1):
@@ -230,17 +169,26 @@ def main_elec_pos(patient, POM_TO_MRIELEC_TRNSFRM=False):
         # Options for flirt co-registration
         regopts = "-cost mutualinfo -dof 12 -searchrz -180 180 -searchry -180 180  -searchrx -180 180"
         # regopts = "-searchrz -20 20 -searchry -20 20 -searchrx -20 20"
-        output, std_out, time = \
-            execute_command("flirt "+ regopts +
-                            " -in " + mrielec +
-                            " -ref " + t1 +
-                            " -omat " + mrielec_to_t1 +
-                            " -out " + mrielec_in_t1, cwd=results_root, shell=True)
+        execute_command("flirt " + regopts +
+                        " -in " + mrielec +
+                        " -ref " + t1 +
+                        " -omat " + mrielec_to_t1 +
+                        " -out " + mrielec_in_t1)
 
     # D. Make the actual transformation from pom/mri_elec space to t1 space
     # Transform the coordinates along (pom file ->) mri_elec -> t1 and save the to text and nifti files
     transform(elec_file_pom, mrielec, t1, elec_file_t1, elec_nii_t1, mrielec_to_t1, aff_transform)
 
+    # # Alternative way:
+    #
+    # execute_command("flirt " + regopts +
+    #                 " -in " + pom_in_mrielec +
+    #                 " -inweight " + pom_in_mrielec +
+    #                 " -ref " + t1 +
+    #                 " -omat " + mrielec_to_t1 +
+    #                 " -out " + elec_nii_t1)
+    # transform(elec_file_mrielec, pom_in_mrielec, t1, elec_file_t1, elec_nii_t1, mrielec_to_t1)
+
 if __name__ == "__main__":
 
-    main_elec_pos("TVB1", False)
+    main_elec_pos("TVB3", False)
