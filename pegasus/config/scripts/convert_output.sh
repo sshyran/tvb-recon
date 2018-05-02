1b9434d3881f30b5206699a463e47404e3331b72
Paula Popa <paula.popa@codemart.ro>
2018-05-02 11:21:35 +0300

    Fixes for missing atlas bug.

--- pegasus/config/scripts/convert_output.sh
+++ pegasus/config/scripts/convert_output.sh
@@ -6,9 +6,11 @@ export FREESURFER_HOME
 export SUBJECTS_DIR
 source ${FREESURFER_HOME}/FreeSurferEnv.sh
 
-if [ $3 == "" ]
+§if [ $# -eq 3 ]
 then
-    python -m tvb.recon.qc.tvb_output -p $PWD $PWD $1 $2 $PWD
+python -m tvb.recon.qc.tvb_output -p $3 $PWD $PWD $1 $2 $PWD
+
 else
-    python -m tvb.recon.qc.tvb_output -p $PWD $PWD $1 $2 $PWD --atlas_suffix=$3
+python -m tvb.recon.qc.tvb_output -p default $PWD $PWD $1 $2 $PWD
+
 fi
