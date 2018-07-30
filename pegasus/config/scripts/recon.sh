30887a4bd37f832bb1fcf44c37f36cd7a6ad893a
popaula937 <paula.popa@codemart.ro>
2018-07-30 13:06:55 +0300

    Keep simple recon-all command until we figure out the problem.

--- pegasus/config/scripts/recon.sh
+++ pegasus/config/scripts/recon.sh
@@ -9,15 +9,15 @@ f=$PWD
 
 # We assume that every case of rerunning should just resume recon-all without overwriting
 # TODO: a proper management of recon-all
-if [ -d "${SUBJECTS_DIR}/$1" ]; then
-    if [ -f "${SUBJECTS_DIR}/$1/scripts/IsRunning.lh+rh" ]; then
-        rm ${SUBJECTS_DIR}/$1/scripts/IsRunning.lh+rh
-    fi
-    echo Resuming recon-all!
-    recon-all -all -no-isrunning -parallel -openmp $3 -s $1
-else
-    recon-all -all -parallel -openmp $3 -s $1 -i $2
-fi
+#if [ -d "${SUBJECTS_DIR}/$1" ]; then
+#    if [ -f "${SUBJECTS_DIR}/$1/scripts/IsRunning.lh+rh" ]; then
+#        rm ${SUBJECTS_DIR}/$1/scripts/IsRunning.lh+rh
+#    fi
+#    echo Resuming recon-all!
+#    recon-all -all -no-isrunning -parallel -openmp $3 -s $1
+#else
+recon-all -all -parallel -openmp $3 -s $1 -i $2
+#fi
 
 cd ${SUBJECTS_DIR}/$1/mri
 cp T1.mgz $f
