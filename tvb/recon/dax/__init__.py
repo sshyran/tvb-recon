a1f083d10d36e510c95fb3a9784af04c63475cda
dionperd <dionperd@gmail.com>
2018-08-04 13:22:46 +0200

    TODO: Suggestion for AtlasSuffix class to make easier references to atlases

--- tvb/recon/dax/__init__.py
+++ tvb/recon/dax/__init__.py
@@ -2,6 +2,13 @@ class Atlas(object):
     DEFAULT = "default"
     A2009S = "a2009s"
 
+
+# TODO: better set properties to the exact atlases names to avoid if statements and easily add more parcellations
+# i.e.:
+# class AtlasSuffix(object):
+#     default = ""
+#     a2009s = ".a2009s"
+# with desired use: atlas_suffix = getattr(AtlasSuffix, current_atlas, None)
 class AtlasSuffix(object):
     DEFAULT = ""
     A2009S = ".a2009s"
